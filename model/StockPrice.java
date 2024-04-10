36d1a9e5d5c250f3ec7368a735f161e6dc8ac258
Alexander Tsetsersky <sanchezlife@tut.by>
2024-04-10 00:59:19 -0300

    added model

---
+++ model/StockPrice.java
@@ -0,0 +1,58 @@
+package model;
+
+public class StockPrice {
+
+    private String date;
+    private double open;
+    private double high;
+    private double low;
+    private double close;
+
+    public StockPrice(String date, double open, double high, double low, double close) {
+        this.date = date;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getOpen() {
+        return open;
+    }
+
+    public void setOpen(double open) {
+        this.open = open;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public double getClose() {
+        return close;
+    }
+
+    public void setClose(double close) {
+        this.close = close;
+    }
+}
---
+++ model/StockProfit.java
@@ -0,0 +1,58 @@
+package model;
+
+public class StockProfit {
+
+    private String buyDate;
+    private double buyPrice;
+    private String sellDate;
+    private double sellPrice;
+    private double profit;
+
+    public StockProfit(String buyDate, double buyPrice, String sellDate, double sellPrice, double profit) {
+        this.buyDate = buyDate;
+        this.buyPrice = buyPrice;
+        this.sellDate = sellDate;
+        this.sellPrice = sellPrice;
+        this.profit = profit;
+    }
+
+    public String getBuyDate() {
+        return buyDate;
+    }
+
+    public void setBuyDate(String buyDate) {
+        this.buyDate = buyDate;
+    }
+
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(double buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public String getSellDate() {
+        return sellDate;
+    }
+
+    public void setSellDate(String sellDate) {
+        this.sellDate = sellDate;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public double getProfit() {
+        return profit;
+    }
+
+    public void setProfit(double profit) {
+        this.profit = profit;
+    }
+}
